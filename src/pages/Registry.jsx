9f28785de538984b8685f0a35514a855dcb29a8d
sriya <sriya@sriyas-MacBook-Air.local>
2026-04-23 09:40:04 -0700

    add line on registry

--- src/pages/Registry.jsx
+++ src/pages/Registry.jsx
@@ -2,6 +2,8 @@ export default function Registry() {
   return (
     <section className="page center">
       <h1>Registry</h1>
+      <p className="event-detail">We kindly request no physical gifts. Your presence and blessings are the greatest gift. For those who wish to give, please consider our registry.</p>
+      <br/>
       <iframe
         src="https://widget.zola.com/v1/widget/registry/ujwalandshivani/html"
         title="Zola Registry"
